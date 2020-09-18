8c3733938064ca842febc145b4174aa5cf17b9a4
edpe <ed@edperkins.co.uk>
2020-09-18 19:19:53 +0100

    test client side render method

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,34 @@
-import * as Tone from "tone";
-
-const synth = new Tone.Synth().toMaster();
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [Tone, setTone] = useState();
+  const [synth, setSynth] = useState();
+  const [sequence, setSequence] = useState();
+  const [start, setStart] = useState(0);
+
+  useEffect(() => {
+    import("tone").then((_Tone) => {
+      setTone(_Tone);
+
+      setSynth(new _Tone.Synth().toMaster());
+    });
+
+    setSequence(
+      new Tone.Sequence(
+        (time, note) => {
+          synth.triggerAttackRelease(note, 0.1, time);
+        },
+        ["C4", ["E4", "D4", "E4"], "G4", ["A4", "G4"]]
+      ).start(0)
+    );
+  }, [setTone, setSynth, setSeq]);
+
   const clickHandler = () => {
-    const seq = new Tone.Sequence(
-      (time, note) => {
-        synth.triggerAttackRelease(note, 0.1, time);
-      },
-      ["C4", ["E4", "D4", "E4"], "G4", ["A4", "G4"]]
-    ).start(0);
     Tone.Transport.start();
+    console.log(seq._events);
   };
 
-  return (
-    <div>
-      <button onClick={clickHandler}>start</button>
-    </div>
-  );
+  return <div>{synth && <button onClick={clickHandler}>start</button>}</div>;
 };
 
 export default Home;
